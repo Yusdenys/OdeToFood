0063ed035dce2c1660d4e6f4df7349b0aa41d387
Yusdenys Perez <yudemendoza@gmail.com>
2022-06-01 22:31:21 -0400

    first commit

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages.Restarurants
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
